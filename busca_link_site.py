c182417b5131fac935e57471fb02e58cd7895ef7
pedroalpacheco <pedro.pacheco.a@gmail.com>
2015-11-17 10:39:28 -0200

    primeirocommit

---
+++ busca_link_site.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 10:18:02 2015
+
+@author: papacheco
+"""
+#Baseado:
+#https://github.com/realpython/python-scripts
+
+import requests
+import re
+import os
+
+# get url
+url = raw_input("Enter a URL (include `http://`): ")
+
+#Arquivo de log
+arq = open("ist-sites.txt","w+")
+
+# Conecta a url
+website = requests.get(url)
+
+# Leitura html
+html = website.text
+
+# re.findall pega todos os links
+links = re.findall('"((http|ftp)s?://.*?)"', html)
+
+arq.writelines(links[0])
+
+# Mostra links
+for link in links:
+    print(link[0])
+    
+    #Grava em arquivos
+    arq.writelines('\n'+link[0])
